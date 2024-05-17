adbd5b164899ef700c809c668f30fa860a9eec8e
npoulain <nicolas.poulain@ac-paris.fr>
2024-05-17 16:20:27 +0200

    Indicateur de suppression des tags.

--- components/my/Table/Tags.vue
+++ components/my/Table/Tags.vue
@@ -8,30 +8,44 @@ const props = defineProps({
 
 const toast = useToast();
 const delTag = (nid, tid) => {
-
   const index = tids.value.indexOf(tid);
   tids.value.splice(index, 1);
+  names.value.splice(index, 1);
+  colors.value.splice(index, 1);
   toast.add({ severity: 'success', summary: 'Tag supprimé', detail: `${nid}: ${tid}`, life: 2000 });
 }
 
 const tids = ref([])
-let names = []
-let colors = []
+const names = ref([])
+const colors = ref([])
 
 if (Object.keys(props.tags).length > 0) {
   tids.value = props.tags.tids.split(',')
-  names = props.tags.names.split(',')
-  colors = props.tags.colors.split(',')
+  names.value = props.tags.names.split(',')
+  colors.value = props.tags.colors.split(',')
 }
 
 </script>
 
 <template>
   <span v-for="(tid, key) in tids" :key="nid + tid">
-    <Button v-tooltip.left="'Cliquer pour supprimer'" removable @click="delTag(nid, tid)">
-      <i class="pi pi-tag pr-2" :style="'color:' + colors[key]"></i>
+    <Button v-tooltip.right="'Supprimer'" removable @click="delTag(nid, tid)" class="removable">
+      <i class="pi pi-tag p-2" :style="'color:' + colors[key]"></i>
       {{ names[key] }}
+      <i class="pi pi-times-circle p-2 hide red"></i>
     </Button>
   </span>
 </template>
 
+
+<style scoped>
+.hide {
+  visibility:hidden;
+}
+.removable:hover .hide {
+  visibility: visible;
+}
+.red {
+  color: red;
+}
+</style>
