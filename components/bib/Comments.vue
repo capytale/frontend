825e3984689fc2a19267b4ba356d080a6769d15c
npoulain <nicolas.poulain@ac-paris.fr>
2024-06-18 17:59:19 +0200

    Pas de commentaires multiples WIP

--- components/bib/Comments.vue
+++ components/bib/Comments.vue
@@ -7,6 +7,10 @@ const props = defineProps({
   required: true
 })
 const visible = ref(false);
+const addComment = ref(false);
+const hasAlreadyCommented = ref(false);
+hasAlreadyCommented.value = props.data.comments && props.data.comments.find(c => c.uid == 4343) != null
+
 
 const bib = useBibStore()
 bib.getComments(props.data)
@@ -61,31 +65,39 @@ const nbstars = computed(() => {
     ({{ nbComms }})
   </div>
   <Dialog v-model:visible="visible" maximizable modal :header="'Header ' + data.nid"
-    :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
+    :breakpoints="{ '1199px': '75vw', '575px': '90vw' }" :style="{ width: '50%' }">
 
     <DataView :value="data.comments">
       <template #list="slotProps">
         <div class="flex flex-col">
-          <FloatLabel class="my-8">
-            <Textarea v-model="value" rows="5" style="width: 100%;" />
-            <label>Votre commentaire</label>
-          </FloatLabel>
-          <div class="flex">
-            <font-awesome icon="balance-scale" /> Éthique
-            <font-awesome icon="glasses" /> Clarté
-            <font-awesome icon="lightbulb" /> Fond
-            <font-awesome icon="recycle" /> Utilisabilité
-            <font-awesome icon="bullseye" /> Pertinence
-            <font-awesome icon="spell-check" /> Apparence
+
+          <div v-if="addComment" class="flex flex-col">
+            <FloatLabel class="my-8">
+              <Textarea v-model="value" rows="5" style="width: 100%;" />
+              <label>Votre commentaire</label>
+            </FloatLabel>
+            <div class="flex">
+              <font-awesome icon="balance-scale" /> Éthique
+              <font-awesome icon="glasses" /> Clarté
+              <font-awesome icon="lightbulb" /> Fond
+              <font-awesome icon="recycle" /> Utilisabilité
+              <font-awesome icon="bullseye" /> Pertinence
+              <font-awesome icon="spell-check" /> Apparence
+            </div>
+            <Button label="Poster" @click="addComment = false"/>
           </div>
-          
+
+          <div v-else-if="!hasAlreadyCommented">
+            <Button label="Ajouter un commentaire" @click="addComment = true"/>
+          </div>
+
 
           <div v-for="(item, index) in slotProps.items" :key="index">
-            <hr class="my-4"/>
+            <hr class="my-4" />
             <div class="card">
-              <Badge :value="initials(item.prenom,item.nom)" size="xlarge" severity="success"></Badge>
+              <Badge :value="initials(item.prenom, item.nom)" size="xlarge" severity="success"></Badge>
               {{ item.nom }} {{ item.prenom }}
-              Il y a {{ timeElapsed(item.created *1000 ) }} 
+              Il y a {{ timeElapsed(item.created * 1000) }}
               <div>
                 <span>Ethique{{ item.eth }}</span>
                 <span>Clarté{{ item.clr }}</span>
