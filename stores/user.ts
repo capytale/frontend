a69521460c3e7289885a01079fa37d161ba490dd
npoulain <nicolas.poulain@ac-paris.fr>
2024-06-19 10:21:33 +0200

    store user

---
+++ stores/user.ts
@@ -0,0 +1,13 @@
+import httpClient from '@capytale/activity.js/backend/capytale/http'
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    user: {}
+  }),
+  actions: {
+    async getUser() {
+      this.user = await fetchCurrentUser()
+
+    },
+  },
+})
