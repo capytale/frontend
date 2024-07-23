490cc8531061afeef02e62eb599a3c3227fd3ef4
Mathieu NICOLAS <mathieu.nicolas@me.com>
2024-07-24 00:30:15 +0200

    deux propositions pour le principe d'affichage des favoris dans ActivityAdd

--- components/my/ActivityAdd.vue
+++ components/my/ActivityAdd.vue
@@ -3,11 +3,17 @@
     <template #title>
       <h2 style="margin:0px">Créer une nouvelle activité
       </h2>
+      <div class="bg-red-300 border-red-500 p-4 m-4 rounded w-fit flex flex-row items-center justify-around">
+      <Checkbox v-model="variant" inputId="variant" :binary="true" />
+      <label for="variant" class="ml-4">Option 1 / Option 2 ?</label>
+      </div>
     </template>
     <template #content>
       <div class="overscroll-x-auto myflex">
         <fieldset>
           <legend class="text-lg">Mes activités favorites</legend>
+          <Button v-if="variant" rounded outlined icon="pi pi-star" @click="togglePopover" />
+          <Popover v-if="variant" ref="popover">
           <a v-for="el of myStore.types.filter(el => myStore.favorites.includes(el.id))" :key="el.id"
             :href="'/web/node/add/activity?type=' + el.id" v-tooltip.bottom="el.name" class="hover:shadow-md">
             <img :src="el.icon.path" class="w-16 inline" />
@@ -15,6 +21,17 @@
               {{ el.name.replace("Robot", "").replace("Carte", "").replace("Pi ", "") }}
             </div>
           </a>
+          </Popover>
+
+          <div v-if="!variant">
+          <a v-for="el of myStore.types.filter(el => myStore.favorites.includes(el.id))" :key="el.id"
+            :href="'/web/node/add/activity?type=' + el.id" v-tooltip.bottom="el.name" class="hover:shadow-md">
+            <img :src="el.icon.path" class="w-16 inline" />
+            <div class="text-sm" v-if="false">
+              {{ el.name.replace("Robot", "").replace("Carte", "").replace("Pi ", "") }}
+            </div>
+          </a>
+          </div>
         </fieldset>
         <fieldset>
           <legend class="text-lg">Voir tout</legend>
@@ -32,6 +49,11 @@ const myStore = useMyStore();
 
 myStore.favorites = await TypeApi.getFavorites(true);
 myStore.types = await useActivities()
+
+const popover = ref();
+const togglePopover = (event) => popover.value.toggle(event);
+
+const variant = ref(false);
 </script>
 
 <style scoped>
