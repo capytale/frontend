cdd62c655754234b9ff8c4207aa1bca21274eea3
npoulain <nicolas.poulain@ac-paris.fr>
2024-07-24 11:28:03 +0200

    Merge des modifs de Mathieu et Nico sur ActivityAdd

--- components/my/ActivityAdd.vue
+++ components/my/ActivityAdd.vue
@@ -3,17 +3,24 @@
     <template #title>
       <h2 style="margin:0px">Créer une nouvelle activité
       </h2>
-      <div class="bg-red-300 border-red-500 p-4 m-4 rounded w-fit flex flex-row items-center justify-around">
-      <Checkbox v-model="variant" inputId="variant" :binary="true" />
-      <label for="variant" class="ml-4">Option 1 / Option 2 ?</label>
-      </div>
+      <!-- <div class="bg-red-300 border-red-500 p-4 m-4 rounded w-fit flex flex-row items-center justify-around"> -->
+      <!-- <Checkbox v-model="variant" inputId="variant" :binary="true" /> -->
+      <!-- <label for="variant" class="ml-4">Option 1 / Option 2 ?</label> -->
+      <!-- </div> -->
     </template>
     <template #content>
       <div class="overscroll-x-auto myflex">
+        <NuxtLink to="/activites" class="hover:shadow-md acti-button">
+            <Button type="submit" label="Voir tout" />
+          </NuxtLink>
         <fieldset>
-          <legend class="text-lg">Mes activités favorites</legend>
-          <Button v-if="variant" rounded outlined icon="pi pi-star" @click="togglePopover" />
-          <Popover v-if="variant" ref="popover">
+<!-- <<<<<<< Updated upstream -->
+<!--           <legend class="text-lg">Mes activités favorites</legend> -->
+<!--           <Button v-if="variant" rounded outlined icon="pi pi-star" @click="togglePopover" /> -->
+<!--           <Popover v-if="variant" ref="popover"> -->
+<!-- ======= -->
+          <legend class="text-lg">Mes activités favorites <i class="pi pi-star-fill text-yellow-400"></i> </legend>
+<!-- >>>>>>> Stashed changes -->
           <a v-for="el of myStore.types.filter(el => myStore.favorites.includes(el.id))" :key="el.id"
             :href="'/web/node/add/activity?type=' + el.id" v-tooltip.bottom="el.name" class="hover:shadow-md">
             <img :src="el.icon.path" class="w-16 inline" />
@@ -21,22 +28,17 @@
               {{ el.name.replace("Robot", "").replace("Carte", "").replace("Pi ", "") }}
             </div>
           </a>
-          </Popover>
+          <!-- </Popover> -->
 
-          <div v-if="!variant">
-          <a v-for="el of myStore.types.filter(el => myStore.favorites.includes(el.id))" :key="el.id"
-            :href="'/web/node/add/activity?type=' + el.id" v-tooltip.bottom="el.name" class="hover:shadow-md">
-            <img :src="el.icon.path" class="w-16 inline" />
-            <div class="text-sm" v-if="false">
-              {{ el.name.replace("Robot", "").replace("Carte", "").replace("Pi ", "") }}
-            </div>
-          </a>
-          </div>
-        </fieldset>
-        <fieldset>
-          <legend class="text-lg">Voir tout</legend>
-          <a href="/activites" v-tooltip.bottom="'Autre'"><i
-              class="p-2 pi pi-plus-circle text-blue-500 text-3xl font-bold"></i></a>
+          <!-- <div v-if="!variant"> -->
+          <!-- <a v-for="el of myStore.types.filter(el => myStore.favorites.includes(el.id))" :key="el.id" -->
+          <!--   :href="'/web/node/add/activity?type=' + el.id" v-tooltip.bottom="el.name" class="hover:shadow-md"> -->
+          <!--   <img :src="el.icon.path" class="w-16 inline" /> -->
+          <!--   <div class="text-sm" v-if="false"> -->
+          <!--     {{ el.name.replace("Robot", "").replace("Carte", "").replace("Pi ", "") }} -->
+          <!--   </div> -->
+          <!-- </a> -->
+          <!-- </div> -->
         </fieldset>
       </div>
     </template>
@@ -53,7 +55,7 @@ myStore.types = await useActivities()
 const popover = ref();
 const togglePopover = (event) => popover.value.toggle(event);
 
-const variant = ref(false);
+// const variant = ref(false);
 </script>
 
 <style scoped>
@@ -65,7 +67,13 @@ const variant = ref(false);
 
 fieldset {
   border: 1px solid #000;
-  padding: 1em;
+  padding: 0.5em 1em 1em 1em;
   border-radius: 5px;
+  /* position: relative; */
+  /* top: -10px; */
+}
+.acti-button {
+  position: relative;
+  top: 2px;
 }
 </style>
