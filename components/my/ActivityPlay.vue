6c4e9b73ff2296291f40a7974edbd69441bec4fd
npoulain <nicolas.poulain@ac-paris.fr>
2024-07-24 16:20:56 +0200

    Redirection après saisie du code de partage OK

--- components/my/ActivityPlay.vue
+++ components/my/ActivityPlay.vue
@@ -2,15 +2,31 @@
 import httpClient from '@capytale/activity.js/backend/capytale/http'
 const my = useMyStore()
 const value = ref(null);
+const toast = useToast();
+const invalid = ref(false)
+const message = ref("Ce code n'est pas valide.");
 
 // pour le backend, voir capytaleUI/codeForm réclamé par un postGetJsonAsync 
 // revoyer le playerURl de activityBunch 
 // pour faire un window.location.href ici et rediriger vers le player
 
 const codeForm = async () => {
-  const url = await httpClient.postGetJsonAsync("/web/c-hdls/api/code-form", { action: "codeForm", code: value.value })
-  console.log("url", url)
+  try {
+    const url = await httpClient.postGetJsonAsync("/web/c-hdls/api/code-form", { action: "codeForm", code: value.value })
+    console.log("url", url)
+    invalid.value = false
+    location.href = url
+  } catch (e) {
+    console.error(e)
+    invalid.value = true
+  };
 }
+
+const clear = () => {
+  invalid.value = false
+}
+
+
 </script>
 
 
@@ -22,9 +38,11 @@ const codeForm = async () => {
     <template #content>
 
       <div class="flex flex-col gap-2">
-        <label for="code">Saisir le code de partage</label>
+        <Message v-if="invalid" severity="error">{{ message }}</Message>
+        <label v-else for="code" class="mt-4">Saisir le code de partage</label>
         <div class="flex flex-wrap align-items-center gap-2">
-          <InputText id="code" v-model="value" aria-describedby="code-help" placeholder="a12b-345678" />
+          <InputText id="code" v-model="value" aria-describedby="code-help" placeholder="a12b-345678" :invalid="invalid"
+            @focus="clear" />
           <Button type="submit" label="Go !" @click="codeForm" />
         </div>
       </div>
