f9c2cc858d0990c15bff36eab2432694b30f9efd
npoulain <nicolas.poulain@ac-paris.fr>
2024-07-25 11:34:45 +0200

    Quelques évolutions sur l'UI

--- components/ActiCard.vue
+++ components/ActiCard.vue
@@ -11,10 +11,12 @@ const star = computed(() => {
   if (myStore.favorites.includes(props.activite.id)) {
     return {
       icon: "pi pi-star-fill text-yellow-400",
+      tt: "Supprimer des favoris"
     };
   } else {
     return {
       icon: "pi pi-star",
+      tt: "Ajouter aux favoris",
     };
   }
 });
@@ -23,10 +25,10 @@ const toggleFav = async () => {
 
   if (myStore.favorites.includes(props.activite.id)) {
     await myStore.removeFavorite(props.activite.id);
-    toast.add({ severity: 'success', summary: 'Suppression effectuée', life: 2000 });
+    // toast.add({ severity: 'success', summary: 'Suppression effectuée', life: 2000 });
   } else {
     await myStore.addFavorite(props.activite.id);
-    toast.add({ severity: 'success', summary: 'Ajout effectué : ', life: 2000 });
+    // toast.add({ severity: 'success', summary: 'Ajout effectué : ', life: 2000 });
   }
 };
 
@@ -46,20 +48,15 @@ const goActi = (event, msg) => {
 </script>
 
 <template>
-  <Card
-    class="cursor-pointer hover:shadow-md"
-    @click="(event) => goActi(event, 'coucou')"
-  >
+  <Card class="cursor-pointer hover:shadow-md" @click="(event) => goActi(event, 'coucou')">
     <template #title>
       <div class="flex flex-row justify-between items-center w-full">
-        <img
-          :src="activite.icon.path"
-          class="w-16"
-        />
+        <img :src="activite.icon.path" class="w-16" />
         <div class="text-lg font-semibold ml-2 grow">
           {{ activite.name }} <sup v-if="activite.beta" class="text-red-500">beta</sup>
         </div>
-        <i :class="'mx-2 text-lg cursor-pointer starspin '+star.icon" @click="toggleFav()"></i>
+        <i :class="'mx-2 text-lg cursor-pointer starspin star ' + star.icon" @click="toggleFav()"
+          v-tooltip.top="{ value: star.tt, showDelay: 300, hideDelay: 0 }"></i>
       </div>
     </template>
     <template #content>
@@ -70,16 +67,23 @@ const goActi = (event, msg) => {
 
 <style>
 .starspin:hover {
-  animation: ping 1s cubic-bezier(0, 0, 0.2, 1) 1;
+  border-radius: 2em;
+  border: 1px solid #facc15;
+  padding: 0.7em;
 
-  @keyframes ping {
-    50% {
-      transform: scale(1.1);
-    }
+  /* animation: ping 1s cubic-bezier(0, 0, 0.2, 1) 1; */
 
-    100% {
-      transform: scale(1.1);
-    }
-  }
+  /* @keyframes ping { */
+  /*   50% { */
+  /*     transform: scale(0.5); */
+  /*   } */
+
+  /*   100% { */
+  /*     transform: scale(2.1); */
+  /*   } */
+}
+
+.star {
+  padding: 0.7em;
 }
 </style>
