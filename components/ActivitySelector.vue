f417cee59c6449ab988cca6e1e180f63e75c5dce
npoulain <nicolas.poulain@ac-paris.fr>
2024-08-18 22:53:10 +0200

    Mini-amélioration de la Card Favoris lorsqu'elle est vide

--- components/ActivitySelector.vue
+++ components/ActivitySelector.vue
@@ -94,70 +94,60 @@ const dispOptions = [{
 const dialogActi = ref(false);
 const dialogGroup = ref("blocs");
 const dispGroups = computed(() => {
-  return myStore.actiSelView == 'groups' && 
-          search.value.length == 0 && 
-          catChoice.value == 'all' && 
-          matChoice.value == 'all';
+  return myStore.actiSelView == 'groups' &&
+    search.value.length == 0 &&
+    catChoice.value == 'all' &&
+    matChoice.value == 'all';
 });
 const test = ref('')
 </script>
 
 <template>
-    <Card class="mb-4 rounded-t-none">
+  <Card class="mb-4 rounded-t-none">
     <template #content>
       <div class="flex flex-row justify-between flex-wrap activites-header">
         <div class="text-3xl font-bold">Créer une nouvelle activité</div>
-          <InputText v-model="search" class="" placeholder="Recherche..." />
+        <InputText v-model="search" class="" placeholder="Recherche..." />
         <div class="flex flex-row gap-4">
           <Select v-model="catChoice" :options="cats" option-label="label" option-value="value" :ui="inputstyle" />
           <!-- <Select v-model="matChoice" :options="mats" option-label="label" option-value="value" :ui="inputstyle" /> -->
         </div>
       </div>
     </template>
-    </Card>
-    <div class="grid lg:grid-cols-4 sm:grid-cols-3 gap-4 mb-4" v-if="dispGroups">
-      <!-- <Card> -->
-      <!--   <template #header><div class="text-center font-bold pt-3">Favoris</div></template> -->
-      <!--   <template #content> -->
-      <!--     <div class="flex flex-wrap"> -->
-      <!--     <a v-for="acti in myStore.favorites" :key="acti" :href="'/web/node/add/activity?type=' + acti" @mouseover="test = acti" @mouseleave="test = ''"> -->
-      <!--       <img -->
-      <!--         :src="myStore.types.find(el => el.id == acti).icon.path" v-tooltip.bottom="acti"  -->
-      <!--         class="w-12 transition-opacity duration-[400ms]" :class="test != '' && test != acti ? 'opacity-20' : ''" -->
-      <!--       /> -->
-      <!--     </a> -->
-      <!--     </div> -->
-      <!--   </template> -->
-      <!-- </Card> -->
-      <Card v-for="(obj, cat, index) in myStore.groupsAvecFav" class="hover:ring-capycolor-400 hover:ring-2 hover:cursor-pointer" @click="dialogActi = true; dialogGroup=cat">
-      <template #header><div class="text-center font-bold pt-3">{{ obj.title }}</div></template>
-      <template #content>
-      <div class="flex flex-wrap">
-        <img
-          :src="myStore.types.find(el => el.id == acti).icon.path" v-for="acti in obj.activities"
-          class="w-12 m-1"
-        />
-      </div>
-      </template>
-      </Card>
-    </div>
-    <Dialog v-model:visible="dialogActi" modal dismissableMask class="md:w-4/6">
+  </Card>
+  <div class="grid lg:grid-cols-4 sm:grid-cols-3 gap-4 mb-4" v-if="dispGroups">
+    <Card v-for="(obj, cat, index) in myStore.groupsAvecFav"
+      class="hover:ring-capycolor-400 hover:ring-2 hover:cursor-pointer" @click="dialogActi = true; dialogGroup = cat">
       <template #header>
-        <div class="text-lg font-bold">Catégorie {{ myStore.groupsAvecFav[dialogGroup].title }} : Choix des activités </div>
+        <div class="text-center font-bold pt-3">{{ obj.title }}</div>
       </template>
-      <div class="grid grid-cols-2 gap-4">
-        <ActiCard v-for="acti in myStore.types.filter(el => myStore.groupsAvecFav[dialogGroup].activities.includes(el.id))" :activite="acti" />
-      </div>
-    </Dialog>
-    <div class="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4" v-if="!dispGroups">
-      <ActiCard
-        v-for="(item, index) of chosenCats
-          .filter((a) => a.score > seuil)
-          .sort((a, b) => b.score - a.score)"
-        :key="index"
-        :activite="item"
-      ></ActiCard>
+      <template #content>
+        <div class="flex flex-wrap">
+          <img :src="myStore.types.find(el => el.id == acti).icon.path" v-for="acti in obj.activities" class="w-12 m-1" />
+
+          <template v-if="obj.title == 'Favoris' && obj.activities.length == 0">
+            <p>Aucune activité favorite. </p>
+            <p>Ajoutez-en en cliquant sur une des icônes <i class="pi pi-star" /> </p>
+          </template>
+
+        </div>
+      </template>
+    </Card>
+  </div>
+  <Dialog v-model:visible="dialogActi" modal dismissableMask class="md:w-4/6">
+    <template #header>
+      <div class="text-lg font-bold">Catégorie {{ myStore.groupsAvecFav[dialogGroup].title }} : Choix des activités </div>
+    </template>
+    <div class="grid grid-cols-2 gap-4">
+      <ActiCard v-for="acti in myStore.types.filter(el => myStore.groupsAvecFav[dialogGroup].activities.includes(el.id))"
+        :activite="acti" />
     </div>
+  </Dialog>
+  <div class="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4" v-if="!dispGroups">
+    <ActiCard v-for="(item, index) of chosenCats
+      .filter((a) => a.score > seuil)
+      .sort((a, b) => b.score - a.score)" :key="index" :activite="item"></ActiCard>
+  </div>
 </template>
 
 <style>
@@ -177,11 +167,13 @@ const test = ref('')
   max-height: 0;
 }
 
-.p-row-even, .p-row-odd {
+.p-row-even,
+.p-row-odd {
   cursor: pointer;
 }
 
-.p-row-even:hover, .p-row-odd:hover {
+.p-row-even:hover,
+.p-row-odd:hover {
   background-color: #f5f5f5;
 }
 
@@ -189,3 +181,4 @@ const test = ref('')
   align-items: center;
 }
 </style>
+
--- components/my/ActivityPlay.vue
+++ components/my/ActivityPlay.vue
@@ -25,7 +25,6 @@ const clear = () => {
   invalid.value = false
 }
 
-
 </script>
 
 
