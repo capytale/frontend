0b2e0087380014a76b853ebf05801a5a1a5ecc2b
Mathieu NICOLAS <mathieu.nicolas@me.com>
2024-08-27 15:47:17 +0200

    marges logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-row items-center justify-around">
-      <img :src="'https://capytale2.ac-paris.fr/logo.svg'" class="w-40 mr-4 rounded-lg" />
+      <img :src="'https://capytale2.ac-paris.fr/logo.svg'" class="w-40 mr-4 rounded-lg my-16" />
       <div>
         <h1 class="text-4xl font-bold">Capytale</h1>
         <h2 class="text-2xl font-semibold">
@@ -8,7 +8,7 @@
         </h2>
       </div>
     </div>
-    <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8 my-32">
+    <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8 mb-32">
       <Card v-for="(c, index) of cards" key="index">
       <template #content>
         <div class="flex flex-row align-center">
