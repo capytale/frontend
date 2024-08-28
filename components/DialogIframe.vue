3c572f6aae555db8bd8415c39e894b8c726bab4c
Mathieu NICOLAS <mathieu.nicolas@me.com>
2024-08-28 19:26:29 +0200

    amélioration DialogIframe

--- components/DialogIframe.vue
+++ components/DialogIframe.vue
@@ -1,6 +1,10 @@
 <template>
-  <Dialog v-if="model" v-model:visible="model" maximizable modal :header="'&nbsp;'" style="width: 80%; height: 80vh"
-  :breakpoints="{ '1199px': '75vw', '575px': '90vw' }" content-class="h-full" dismissableMask>
+  <Dialog v-if="model" v-model:visible="model" :header="props.titre || '&nbsp;'" style="width: 80%; height: 80vh"
+  :breakpoints="{ '1199px': '75vw', '575px': '90vw' }" content-class="h-full" maximizable modal dismissableMask>
+  <template #header>
+    <slot name="header" />
+  </template>
+  <slot />
     <iframe :src="props.url" width="100%" height="100%"></iframe>
   </Dialog>
 </template>
