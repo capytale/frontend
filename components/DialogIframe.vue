de7060b7842518002804e2b73d14cc5f005027ab
Mathieu NICOLAS <mathieu.nicolas@me.com>
2024-08-28 16:44:26 +0200

    création composant DialogIframe

---
+++ components/DialogIframe.vue
@@ -0,0 +1,16 @@
+<template>
+  <Dialog v-model:visible="model" maximizable modal :header="'&nbsp;'" style="width: 80%; height: 80vh"
+  :breakpoints="{ '1199px': '75vw', '575px': '90vw' }" content-class="h-full" dismissableMask>
+    <iframe :src="props.url" width="100%" height="100%"></iframe>
+  </Dialog>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  url?: string
+  titre?: string
+  icon?: string
+}>()
+
+const model = defineModel<boolean>()
+</script>
