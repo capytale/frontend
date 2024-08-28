a0f9dcff8238ac952894d678bd0bca45dc095632
Mathieu NICOLAS <mathieu.nicolas@me.com>
2024-08-28 16:00:01 +0200

    capycolor is back !

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,24 @@ import Lara from '@primevue/themes/lara';
 import { definePreset } from '@primevue/themes';
 
 const capyPreset = definePreset(Lara, {
+  primitive: {
+    capycolor: {'50': '#f3f7fc','100': '#e5eef9','200': '#c6dcf1','300': '#93bfe6','400': '#5a9ed6','500': '#327bb8','600': '#2567a4','700': '#1f5285','800': '#1d466f','900': '#1d3c5d','950': '#14273d' }
+  },
+  semantic: {
+    primary: {
+      50: '{emerald.50}',
+      100: '{capycolor.100}',
+      200: '{capycolor.200}',
+      300: '{capycolor.300}',
+      400: '{capycolor.400}',
+      500: '{capycolor.500}',
+      600: '{capycolor.600}',
+      700: '{capycolor.700}',
+      800: '{capycolor.800}',
+      900: '{capycolor.900}',
+      950: '{capycolor.950}'
+    },
+  },
   components: {
     card: {
       colorScheme: {
