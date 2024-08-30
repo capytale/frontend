17ad8cfc4540ffacefedc1e85ad6069e6dc6fd95
Nicolas Poulain <nicolas.poulain@ac-paris.fr>
2024-08-30 09:57:02 +0200

    Clonage depuis la bib. OK.

--- components/bib/ViewActivity.vue
+++ components/bib/ViewActivity.vue
@@ -1,6 +1,26 @@
 <script setup>
 import { decode } from 'html-entities';
 const toast = useToast();
+const activites = useActivitiesStore()
+
+const backHomeVisible = ref(false);
+const patience = ref(false);
+
+const backToHome = () => {
+  navigateTo('/my')
+  patience.value = true
+}
+
+const clone = async (nid) => {
+  try {
+    const response = await activites.cloneActivity(nid)
+    toast.add({ severity: 'success', summary: 'Clonage réussi ', life: 2000 });
+    backHomeVisible.value = true
+  }
+  catch (e) {
+    toast.add({ severity: 'error', summary: 'Échec du clonage : ', detail: `nid = ${nid} - ${e}` });
+  }
+}
 
 const props = defineProps({
   data: Object,
@@ -31,7 +51,9 @@ const copy = (nid) => {
           <div>
             <Button @click="copy(props.data.nid)" label="Copier le lien" icon="pi pi-link" class="mx-2"
               v-tooltip.bottom="{ value: 'Ce lien permet d\'accéder directement à cette ressource partagée. Cliquez pour copier', showDelay: 300, hideDelay: 100 }" />
-            <Button label="Cloner" icon="pi pi-clone" class="mx-2"
+            <Button v-if="backHomeVisible"  @click="backToHome" label="Voir le clone" :icon="patience ? 'pi pi-spin pi-spinner' : 'pi pi-chevron-left' " class="mx-2"
+              v-tooltip.bottom="{ value: 'Revenir à la page Mes activités', showDelay: 300, hideDelay: 100 }" />
+            <Button v-else @click="clone(props.data.nid)" label="Cloner" icon="pi pi-clone" class="mx-2"
               v-tooltip.bottom="{ value: 'Cloner cette ressource partagée', showDelay: 300, hideDelay: 100 }" />
           </div>
         </div>
