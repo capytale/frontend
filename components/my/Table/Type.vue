17bee4e8765d398effa415eedc2a7e5ea8b67ed6
tjaisson <thomas.jaisson@ac-paris.fr>
2024-09-03 16:58:57 +0200

    icônes des activités de la page my

--- components/my/Table/Type.vue
+++ components/my/Table/Type.vue
@@ -1,15 +1,22 @@
 <script setup>
-import { typeIcon } from '~/utils/format';
-const my = useMyStore()
+import { useActivityTypesList } from "~/composables/activityTypes/list";
+
+const atl = useActivityTypesList();
 
 const props = defineProps({
   data: Object,
 })
 
+const dispData = computed(() => {
+  return {
+    name: atl.getTypeInfo(props.data.type)?.name,
+    icon: atl.getTypeInfo(props.data.type)?.icon.path,
+  }
+})
+
 </script>
 
 <template>
-  <img :src="typeIcon(props.data.type, my)" class="w-12" v-tooltip.top="{ value: props.data.type, showDelay: 400, hideDelay: 0 }" 
+  <img :src="dispData.icon" class="w-12" v-tooltip.top="{ value: dispData.name, showDelay: 400, hideDelay: 0 }"
     type="text" placeholder="Right" :alt="'Activité' + props.data.type">
 </template>
-
