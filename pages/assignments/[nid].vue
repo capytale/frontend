2fe7505f0549c75a2475e969b7872560c309f39a
tjaisson <thomas.jaisson@ac-paris.fr>
2024-09-05 14:18:41 +0200

    Minor ts syntax

--- pages/assignments/[nid].vue
+++ pages/assignments/[nid].vue
@@ -2,13 +2,13 @@
 const $route = useRoute();
 definePageMeta({
     validate({ params }) {
-        return /^\d+$/.test(params.nid)
+        return /^\d+$/.test(params.nid as string)
     },
 })
 </script>
 
 <template>
     <div class="mt-4">
-        <AssignmentStudentList :nid="$route.params.nid" />
+        <AssignmentStudentList :nid="$route.params.nid as string" />
     </div>
 </template>
