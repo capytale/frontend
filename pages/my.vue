2a32cd1184c2af8bc4933a2d3269c290bde31a14
Mathieu NICOLAS <mathieu.nicolas@me.com>
2024-09-09 20:15:11 +0200

    masquage du sideMenu et de l'icône hover si la liste des activités est vide

--- pages/my.vue
+++ pages/my.vue
@@ -1,6 +1,7 @@
 <script setup>
 document.title = "Capytale"
 const sideMenu = useSideMenuStore()
+const activites = useActivitiesStore()
 
 // Possible interaction dynamique avec le viewport pour auto-hide du menu
 import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
@@ -26,8 +27,14 @@ const handleResize = (event) => {
 // TODO : récupérer la dernière size dans les prefs utilisateur
 const size = [25,75]
 
+const sideState = computed(() => {
+  if(activites.activities.data?.length === 0) return "empty";
+  if(bplgAndSmaller.value) return "collapsed";
+  return "visible";
+})
+
 const gutter = computed(() => {
-  return sideMenu.visible ? 2 : 0
+  return sideMenu.visible && sideState.value !== 'empty' ? 2 : 0
 })
 </script>
 
@@ -38,13 +45,13 @@ const gutter = computed(() => {
       <MyActivityAdd />
     </div>
     <Splitter @resizeend="handleResize" class="relative" :gutterSize="gutter">
-    <SplitterPanel :size="size[0]" v-show="sideMenu.visible">
+    <SplitterPanel :size="size[0]" v-show="sideMenu.visible && sideState !== 'empty'">
       <SideMenu />
     </SplitterPanel>
     <SplitterPanel :size="size[1]">
     <div class="flex flex-row overflow-scroll">
       <div>
-      <div v-if="!sideMenu.visible" class="mt-16 h-16 w-16 rounded-full border-2 flex justify-center items-center absolute top-0 -left-8 z-[99]" style="background-color: var(--p-card-background)" @mouseover="sideMenu.hover = true">
+      <div v-if="!sideMenu.visible && sideState !== 'empty'" class="mt-16 h-16 w-16 rounded-full border-2 flex justify-center items-center absolute top-0 -left-8 z-[99]" style="background-color: var(--p-card-background)" @mouseover="sideMenu.hover = true">
       <i class="pi pi-tags" style="font-size: 1.5rem;"></i>
       </div>
       <div v-if="sideMenu.hover && !sideMenu.visible" class="absolute top-24 left-6 z-[100] p-card" @mouseleave="sideMenu.hover = false">
