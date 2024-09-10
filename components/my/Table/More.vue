a450de3931cb6ab30fcc86ae5154dfd69e151dee
tjaisson <thomas.jaisson@ac-paris.fr>
2024-09-10 10:30:11 +0200

    fix: affichage des params par le menu more

--- components/my/Table/More.vue
+++ components/my/Table/More.vue
@@ -144,10 +144,7 @@ const toggle = (event) => {
     <Menu ref="menu" id="overlay_menu" :model="items" :popup="true" />
   </div>
 
-  <Dialog v-model:visible="visible" maximizable modal :header="'&nbsp;'" style="width: 80%; height: 80vh"
-    :breakpoints="{ '1199px': '75vw', '575px': '90vw' }" dismissableMask>
-    <MyActivityEdit :data="props.data" @close="visible = false" />
-  </Dialog>
+  <MyActivityEdit v-model="visible" :data="props.data" />
 </template>
 
 <style scoped>
