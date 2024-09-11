52b0609ec080fe51779f49c010a428fdb93d7b2c
npoulain <nicolas.poulain@ac-paris.fr>
2024-09-11 12:28:45 +0200

    Newbie Élève v1

--- components/my/WelcomeNewbie.vue
+++ components/my/WelcomeNewbie.vue
@@ -10,22 +10,18 @@ const events = ref([
   { status: '1. Créer une activité', 
     icon: 'pi pi-sparkles', 
     color: '#327BB8', 
-    float: 'left', 
     img: 'media/etape1.png', 
     content: 'Vous pouvez consulter la bibliothèque pour réutiliser une activité partagée ou en créer une nouvelle parmi celles disponibles : Python, GeoGebra, Scratch ou MathAléa mais aussi MicroBit, Arduino ou programmation de robots.'
   },
   { status: '2. Distribuer à la classe', 
     icon: 'pi pi-users', 
     color: '#F46036', 
-    float: 'left', 
     img: 'media/etape2.png', 
     content: "Votre activité possède un code de partage que vous donnez aux élève pour leur permettre de créer des copies à partir de votre activité." 
   },
   { status: '3. Évaluer les travaux', 
     icon: 'pi pi-check-circle',
     color: '#22c55e', 
-    float: 'left', 
-    // img: 'https://cdn.ac-paris.fr/capytale/media/4_3_qualite28_GeoGebra.png', 
     img: 'media/etape3.png', 
     content: "Vous consultez et évaluez les copies de vos élèves qui arrivent automatiquement." 
   },
@@ -38,6 +34,7 @@ const events = ref([
     <h1>Bienvenue sur Capytale</h1>
 
     <p class="mt-8 mb-10">
+      Vous n'avez encore aucune activité. Voici les trois étapes pour travailler le codage ou les sciences en général avec vos élèves.
     </p>
 
     <Timeline :value="events" class="customized-timeline">
@@ -61,39 +58,26 @@ const events = ref([
         </Card>
       </template>
       <template #content="slotProps">
-        <img :src="slotProps.item.img" :class="'cropped-image mt-6 ' + slotProps.item.float">
+        <img :src="slotProps.item.img" class="image mt-6 mb-4">
       </template>
     </Timeline>
-    <p>
-      Pour commencer, vous pouvez créer une nouvelle activité ci-dessus ou bien consulter la bibliothèque d'activités
-      partagées entre enseignants.
-    </p>
-
 
   </template>
   <template v-else>
-    Welcome newbie student !
+    <h1>Bienvenue sur Capytale</h1>
+
+    <p class="mt-8 mb-10">
+      Vous n'avez encore aucune activité. Si votre enseignant vous a donné un code, saisissez-le et cliquez sur le bouton "Go !".
+    </p>
+
   </template>
 </template>
 
 
 <style scoped>
-.cropped-image {
-  /* width: 80%; */
-  /* height: 150px; */
+.image {
   border-radius: 10px;
-  /* object-fit: cover; */
-  /* object-position: 0% 0%; */
-  /* border: 0px solid black; */
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
-}
-
-.right {
-  float: right;
-}
-
-.left {
-  float: left;
 }
 </style>
 
