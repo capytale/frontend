a524f3ddc734ac52984bc6e770bfe03aefe10524
tjaisson <thomas.jaisson@ac-paris.fr>
2024-09-12 13:47:45 +0200

    Fix: remove italic

--- components/my/Table/Changed.vue
+++ components/my/Table/Changed.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import { timeElapsed } from '~/utils/format';
 
-const props = defineProps<{data: {changed: number|string}}>()
+const props = defineProps < { data: { changed: number | string } } > ()
 
 </script>
 
 <template>
   <div>
-    <i>Il y a {{ timeElapsed(data.changed * 1000) }}</i>
+    <span class='whitespace-nowrap'>Il y a {{ timeElapsed(data.changed * 1000) }}</span>
   </div>
 </template>
-
