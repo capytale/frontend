73db51b29e7a05b5fde81447a75472741b204964
tjaisson <thomas.jaisson@ac-paris.fr>
2024-09-12 12:36:15 +0200

    'changed' est calculé côté backend

--- components/my/Table/Changed.vue
+++ components/my/Table/Changed.vue
@@ -1,28 +1,13 @@
-<script setup>
+<script setup lang="ts">
 import { timeElapsed } from '~/utils/format';
 
-const props = defineProps({
-  data: Object,
-})
-
-// const activites = useActivitiesStore()
-// activites.getAllDetails(props.data)
-
-const computedChanged = computed(() => {
-  if (!props.data.extra) return props.data.changed 
-  if (props.data.saMaxChanged == null) return props.data.changed
-  // return `${props.data.saMaxChanged}-${props.data.changed}`
-  return Math.max(props.data.changed, props.data.saMaxChanged)
-})
+const props = defineProps<{data: {changed: number|string}}>()
 
 </script>
 
 <template>
-  <div v-if="!props.data.extra">
+  <div>
     <i>Il y a {{ timeElapsed(data.changed * 1000) }}</i>
-  </div>
-  <div v-else>
-    <span class='whitespace-nowrap'>Il y a {{ timeElapsed(computedChanged * 1000) }}</span>
   </div>
 </template>
 
