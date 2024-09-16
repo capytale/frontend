b8c61028c6603dcc4674a808c1431927b7ff127e
npoulain <nicolas.poulain@ac-paris.fr>
2024-09-16 16:34:41 +0200

    Passage à la ligen autorisé pour gagner de la largeur sur le tableau My

--- components/my/Table/Changed.vue
+++ components/my/Table/Changed.vue
@@ -8,6 +8,6 @@ const props = defineProps < { data: { changed: number | string } } > ()
 <template>
   <div>
     <!-- <span class='whitespace-nowrap'>Il y a {{ timeElapsed(data.changed * 1000) }}</span> -->
-    <span class='whitespace-nowrap'>{{ formatPrettyDateTime(data.changed * 1000) }}</span>
+    <span class=''>{{ formatPrettyDateTime(data.changed * 1000) }}</span>
   </div>
 </template>
