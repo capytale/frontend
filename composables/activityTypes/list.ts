5344ec16aa0484bef7440966f43ea812e40abf71
tjaisson <thomas.jaisson@ac-paris.fr>
2024-09-18 18:44:12 +0200

    activity types: documentation de useActivityTypesList

--- composables/activityTypes/list.ts
+++ composables/activityTypes/list.ts
@@ -18,9 +18,13 @@ const unknownType: ActivityType = {
 };
 
 const status = shallowRef<'loading' | 'loaded' | 'error'>('loading');
+
 const error = shallowRef<any>();
+
 const typeDico = shallowRef<{ [t: string]: ActivityType }>({});
+
 const list = shallowRef<string[]>([]);
+
 let fetchPromise: Promise<void> | null = null;
 
 function reload(): void {
@@ -48,20 +52,53 @@ function reload(): void {
     });
 }
 
+/**
+ * Fournit les infos sur le type d'activité.
+ * Renvoie une valeur réactive.
+ * Tant que la liste des types n'est pas chargée depuis le backend, renvoie unknownType.
+ * 
+ * @param type le type d'activité
+ * @returns une valeur réactive contenant les informations sur le type
+ */
 function getTypeInfo(type: string): ActivityType {
   if (typeExists(type)) return typeDico.value[type];
   else return unknownType;
 }
 
+/**
+ * Fournit l'URL de création d'une activité du type donné.
+ * Renvoie une valeur réactive qui est '' tant que la liste des types n'est pas chargée
+ * depuis le backend.
+ * Renvoie '' si le type n'existe pas.
+ * 
+ * @param type le type d'activité
+ * @returns une valeur réactive contenant l'URL de création
+ */
 function getCreateUrl(type: string): string {
   if (typeExists(type)) return typeApi.getCreateUrl(getTypeInfo(type));
   else return '';
 }
 
+/**
+ * Indique si le type existe
+ * Renvoie une valeur réactive qui est false tant que la liste des types n'est pas chargée
+ * depuis le backend.
+ * 
+ * @param type le type à tester
+ * @returns valeur réactive indiquant si le type existe
+ */
 function typeExists(type: string): boolean {
   return !!typeDico.value[type];
 }
 
+/**
+ * Indique si le type est disponible pour l'utilisateur courant.
+ * Renvoie une valeur réactive qui est false tant que la liste des types n'est pas chargée
+ * depuis le backend.
+ * 
+ * @param type le type à tester
+ * @returns valeur réactive indiquant si le type est disponible
+ */
 function typeIsAvailable(type: string): boolean {
   return !!getTypeInfo(type)?.available;
 }
@@ -72,13 +109,30 @@ export const useActivityTypesList = () => {
   if (status.value !== 'loaded') reload();
   return readonly({
     reload,
+    /**
+     * Liste complète des types d'activité. Y compris les types non disponibles
+     * pour l'utilisateur courant.
+     * Valeur réactive.
+     */
     list,
+    /**
+     * Liste des types d'activité disponibles pour l'utilisateur courant.
+     * Valeur réactive.
+     */
     availableTypes,
     getTypeInfo,
     getCreateUrl,
     typeExists,
     typeIsAvailable,
+    /**
+     * Statut du chargement de la liste des types d'activité.
+     * Valeur réactive.
+     */
     status,
+    /**
+     * Erreur lors du chargement de la liste des types d'activité.
+     * Valeur réactive.
+     */
     error,
   })
 };
