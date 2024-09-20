14c895b1e4d876e9fa2e6861e9404b020f17c45c
tjaisson <thomas.jaisson@ac-paris.fr>
2024-09-20 11:57:26 +0200

    Colonne 'evaluation' pour activité perso élève => vide

--- components/my/Table/EvaluationActivity.vue
+++ components/my/Table/EvaluationActivity.vue
@@ -8,14 +8,13 @@ const props = defineProps({
 const visible = ref(false);
 const my = useMyStore()
 
-
 const pluriel = (s) => {
   return s > 1 ? 's' : ''
 }
 
 const details = computed(() => {
-  if (!props.isTeacher)
-    return "Activité personnelle"
+  if (!props.isTeacher) return null
+
   const arch = props.data.viewsTotal - props.data.viewsDetails.visible
   const a = props.data.viewsDetails["100"]
   const b = props.data.viewsDetails["200"]
@@ -26,9 +25,9 @@ const details = computed(() => {
     + arch + " archivée" + pluriel(arch)
 })
 const nbViews = computed(() => {
-  if (!props.isTeacher)
-    return "Activité personnelle"
-  else if (props.data.viewsDetails.visible == 0)
+  if (!props.isTeacher) return null
+
+  if (props.data.viewsDetails.visible == 0)
     return "Aucune copie"
   else
     return props.data.viewsDetails.visible > 1 ? props.data.viewsDetails.visible + ' copies' : props.data.viewsDetails.visible + ' copie'
@@ -40,23 +39,31 @@ const testAssignments = () => {
 </script>
 
 <template>
-  <div v-if="props.data.extra">
-    <Button v-if="data.viewsTotal == 0" severity="secondary" text disabled>Aucune copie</Button>
-    <Button v-else @click="testAssignments" severity="primary"
-      v-tooltip.top="{ value: details, showDelay: 400, hideDelay: 0 }" text> {{ nbViews }} </Button>
-    <Dialog v-model:visible="visible" position="top" maximizable modal header="&nbsp;" :style="{ width: '90%' }"
-      dismissableMask>
-      <template #header v-if="my.loadingAssignments">
-        <Skeleton shape="circle" size="4rem" class="mr-2 my-2"></Skeleton>
-        <Skeleton width="20rem" class="mb-2"></Skeleton>
-      </template>
-      <template #header v-else>
-        <img :src="my.assignments.icon" alt="icon" class="w-16 h-16" />
-        <span class="activity-title">{{ my.assignments.title }}</span>
-      </template>
-      <AssignmentStudentList :nid="props.data.nid" :viewsVisible="parseInt(data.viewsDetails.visible)" />
-    </Dialog>
-  </div>
+  <template v-if="props.isTeacher">
+    <!-- vue teacher -->
+    <div v-if="props.data.extra">
+      <Button @click="testAssignments" severity="primary"
+        v-tooltip.top="{ value: details, showDelay: 400, hideDelay: 0 }" text> {{ nbViews }} </Button>
+      <Dialog v-model:visible="visible" position="top" maximizable modal header="&nbsp;" :style="{ width: '90%' }"
+        dismissableMask>
+        <template #header v-if="my.loadingAssignments">
+          <Skeleton shape="circle" size="4rem" class="mr-2 my-2"></Skeleton>
+          <Skeleton width="20rem" class="mb-2"></Skeleton>
+        </template>
+        <template #header v-else>
+          <img :src="my.assignments.icon" alt="icon" class="w-16 h-16" />
+          <span class="activity-title">{{ my.assignments.title }}</span>
+        </template>
+        <AssignmentStudentList :nid="props.data.nid" :viewsVisible="parseInt(data.viewsDetails.visible)" />
+      </Dialog>
+    </div>
+    <div v-else>
+      <!-- Chargement des détails -->
+    </div>
+  </template>
+  <template v-else>
+    <!-- vue student (activité perso) -->
+  </template>
 </template>
 
 <style scoped>
