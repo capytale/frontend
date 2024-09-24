79aa546d98f30cde206a9b8a0c3a333968976fa0
tjaisson <thomas.jaisson@ac-paris.fr>
2024-09-24 13:59:01 +0200

    Couleur des évaluations basée sur 3 couleurs: min, mid & max

--- utils/getStyle.ts
+++ utils/getStyle.ts
@@ -1,12 +1,31 @@
-export const getStyle = function (score, scoreMax) {
-  // Couleurs de  ff0000 -> ffff00 -> 00ff00
-  // pour Score/scoreMax de 
-  //              0      -> 0.5    -> 1
-    const r = Math.min(220, 510 - parseInt(510*score/scoreMax));
-    const g = Math.min(220, parseInt(510*score/scoreMax));
-    const b = 0;
-    const idle = ''
-    if (score != null) return `background-color: rgba(${r}, ${g}, ${b}, 0.7);`
-    else
-    return idle
+type Color = { r: number, g: number, b: number };
+
+/** couleur pour 0/10 */
+const nullColor = { r: 220, g: 20, b: 60 };
+/** couleur pour 5/10 */
+const midColor = { r: 255, g: 165, b: 0 };
+/** couleur pour 10/10 */
+const maxColor = { r: 0, g: 128, b: 0 };
+
+function interpolColors(s: number, c1: Color, c2: Color): Color {
+  return {
+    r: c1.r + (c2.r - c1.r) * s,
+    g: c1.g + (c2.g - c1.g) * s,
+    b: c1.b + (c2.b - c1.b) * s
   }
+}
+
+/**
+ * Retourne un style css pour afficher une couleur en fonction d'un score
+ */
+export function getStyle(score: number | null, scoreMax: number): string {
+  if ((score == null) || (scoreMax == 0)) return 'background-color: AliceBlue; color: DarkGray;'
+  /** score normalisé entre 0 et 2 */
+  const normalizedScore = 2 * score / scoreMax;
+  const c: Color = (normalizedScore < 1) ?
+    interpolColors(normalizedScore, nullColor, midColor) :
+    interpolColors((normalizedScore - 1), midColor, maxColor);
+  const intensity = c.r * 0.299 + c.g * 0.587 + c.b * 0.114;
+  const color = intensity > 186 ? 'black' : 'white';
+  return `background-color: rgb(${Math.floor(c.r)}, ${Math.floor(c.g)}, ${Math.floor(c.b)}); color: ${color};`
+}
