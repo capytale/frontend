29904bce8b9befc9e1f3d9474edf68eaf0532747
Nicolas Poulain <nicolas.poulain@ac-paris.fr>
2024-09-25 00:42:01 +0200

    Navigation aPeUpRé correcte :-/

--- pages/index.vue
+++ pages/index.vue
@@ -1,80 +1,3 @@
-<template>
-  <article>
-    <div class="bottomRight">
-      <span @click="nextSnap"
-        class="slidedown-icon h-8 w-8 bg-primary text-primary-contrast rounded-full inline-flex items-center justify-center">
-        <i :class="upDownIcon" />
-      </span>
-    </div>
-
-    <section>
-      <div class="flex flex-row items-center justify-around">
-        <img :src="'https://capytale2.ac-paris.fr/logo.svg'" class="w-40 mr-4 rounded-lg my-16" />
-        <div>
-          <h1>Capytale</h1>
-          <h2>
-            Activités numériques et de programmation pour la classe
-          </h2>
-        </div>
-      </div>
-      <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8 mb-32">
-        <Card v-for="(c, index) of cards_OLD" key="index">
-          <template #content>
-            <div class="flex flex-row align-center">
-              <i :class="c.icon" class="mt-2 mr-2 text-gray-500"></i>
-              <div class="font-semibold">{{ c.title }}</div>
-            </div>
-            <div class="mt-2 text-gray-500" v-html="c.description"></div>
-          </template>
-        </Card>
-      </div>
-    </section>
-
-    <section>
-      <div class="flex flex-row items-center justify-around">
-        <img :src="'https://capytale2.ac-paris.fr/logo.svg'" class="w-40 mr-4 rounded-lg my-16" />
-        <div>
-          <h1>Capytale Duplicata en réécriture</h1>
-          <h2>
-            Activités numériques et de programmation pour la classe
-          </h2>
-        </div>
-      </div>
-      <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8 mb-32">
-        <Card v-for="(c, index) of cards" key="index">
-          <template #content>
-            <div class="flex flex-row align-center">
-              <i :class="c.icon" class="mt-2 mr-2 text-gray-500"></i>
-              <div class="font-semibold">{{ c.title }}</div>
-            </div>
-            <div class="mt-2 text-gray-500" v-html="c.description"></div>
-          </template>
-        </Card>
-      </div>
-    </section>
-
-    <section>
-      <div class="flex flex-row items-center justify-around">
-        <img :src="'https://capytale2.ac-paris.fr/logo.svg'" class="w-40 mr-4 rounded-lg my-16" />
-        <div>
-          <h2>Créer, distribuer et évaler des activités pédagogiques</h2>
-        </div>
-      </div>
-      <CapyTimeline />
-    </section>
-
-    <section>
-      <div class="flex flex-row items-center justify-around">
-        <img :src="'https://capytale2.ac-paris.fr/logo.svg'" class="w-40 mr-4 rounded-lg my-16" />
-        <div>
-          <h2>Une sélection d'outils complète et cohérente</h2>
-        </div>
-      </div>
-      <CarouselEdu />
-    </section>
-  </article>
-</template>
-
 <script setup>
 // TODO : surveiller les références aux dépendances (composables ?)
 const cards_OLD = [
@@ -144,48 +67,31 @@ const cards_OLD = [
   {
     title: "Commun numérique",
     description:
-      "Soutenu par le ministère de l'éducation. Des partenariats avec des enseignants et l’Edtech. Hébergé sur sur une infrastructure académique. Respectueux du RGPD.",
+      "Soutenu par le ministère de l'éducation.<br/>Des partenariats avec des enseignants développeurs et l’Edtech.<br/>Hébergé sur sur une infrastructure académique et respectueux du RGPD.",
     // icon: "heroicons:user-group",
     icon: "pi pi-users",
   },
   {
     title: "Ressources éducatives libres",
     description:
-      "Une bibliothèque collaborative offrant des milliers de ressources pédagogiques sous licence Creative Commons By-SA.",
+      "Une bibliothèque collaborative offrant des milliers de ressources pédagogiques sous licence Creative Commons By-SA. Accès via tous les ENT de France.<br>Des centaines de milliers d'utilisateurs.",
     // icon: "i-heroicons-lock-open",
     icon: "pi pi-lock-open",
   },
   {
     title: "Environnement simple et homogène",
     description:
-      "Tout pour les STIAM (Sciences, Technologie, Ingénierie, Arts et Mathématiques) pour tous les élèves su cycle 3 aux CPGE. Programmation par blocs (Scratch), en Python, SQL, OCaml, HTML, CSS, JavaScript, ...",
+      "Tout pour les STIAM (Sciences, Technologie, Ingénierie, Arts et Mathématiques) pour les élèves su cycle 3 aux CPGE.",
     // icon: "i-heroicons-academic-cap",
     icon: "pi pi-cog",
   },
   {
     title: "Fluidité pédagogique",
-    description: "Conçu pour simplifier les échanges entre les enseignants et les élèves.",
+    description: "Conçu pour simplifier les échanges entre les enseignants et les élèves.<br/>Fonctionne simplement dans le navigateur de tout ordinateur, tablette ou téléphone.",
     // icon: "i-heroicons-arrows-right-left",
     icon: "pi pi-arrow-right-arrow-left",
   },
-  {
-    title: "En ligne sans installation",
-    description:
-      "Fonctionne simplement dans le navigateur de tout ordinateur, tablette ou téléphone.",
-    // icon: "i-heroicons-globe-alt",
-    icon: "pi pi-globe",
-  },
-  {
-    title: "Adoption massive",
-    description:
-      "Accès via tous les ENT de France.<br>Des centaines de milliers d'utilisateurs.",
-    icon: "pi pi-users",
-    // icon: "i-heroicons-presentation-chart-bar",
-    icon: "pi pi-chart-line",
-  },
 ];
-
-const upDownIcon = ref("pi pi-arrow-down")
 
 const nextSnap = () => {
   const scroller = document.querySelector(".scroller");
@@ -194,22 +100,113 @@ const nextSnap = () => {
   let nextSnap = 0;
   for (let i = 0; i < sections.length; i++) {
     const section = sections[i];
-    console.log("loop", section.offsetTop, currentScroll);
-    upDownIcon.value = "pi pi-arrow-down"
+    // console.log("loop", section.offsetTop, currentScroll);
 
     if (section.offsetTop > currentScroll + 1) {
       nextSnap = section.offsetTop;
-      console.log("i", i, sections.length)
-      upDownIcon.value = i == sections.length - 1 ? "pi pi-arrow-up" : "pi pi-arrow-down"
+      // console.log("i", i, sections.length)
       break;
     }
   }
-  console.log(nextSnap);
+  // console.log(nextSnap);
 
   window.scrollTo({ top: nextSnap, behavior: "smooth" });
 };
 
 </script>
+
+
+<template>
+  <article>
+    <!-- <div class="bottomRight"> -->
+    <!--   <span @click="nextSnap" -->
+    <!--     class="slidedown-icon h-8 w-8 bg-primary text-primary-contrast rounded-full inline-flex items-center justify-center"> -->
+    <!--     <i :class="upDownIcon" /> -->
+    <!--   </span> -->
+    <!-- </div> -->
+
+    <!-- <section> -->
+    <!--   <div class="flex flex-row items-center justify-around"> -->
+    <!--     <img :src="'https://capytale2.ac-paris.fr/logo.svg'" class="w-40 mr-4 rounded-lg my-16" /> -->
+    <!--     <div> -->
+    <!--       <h1>Capytale</h1> -->
+    <!--       <h2> -->
+    <!--         Activités numériques et de programmation pour la classe -->
+    <!--       </h2> -->
+    <!--     </div> -->
+    <!--   </div> -->
+    <!--   <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8 mb-32"> -->
+    <!--     <Card v-for="(c, index) of cards_OLD" key="index"> -->
+    <!--       <template #content> -->
+    <!--         <div class="flex flex-row align-center"> -->
+    <!--           <i :class="c.icon" class="mt-2 mr-2 text-gray-500"></i> -->
+    <!--           <div class="font-semibold">{{ c.title }}</div> -->
+    <!--         </div> -->
+    <!--         <div class="mt-2 text-gray-500" v-html="c.description"></div> -->
+    <!--       </template> -->
+    <!--     </Card> -->
+    <!--   </div> -->
+    <!--   <span @click="nextSnap" -->
+    <!--     class="slidedown-icon h-8 w-8 bg-primary text-primary-contrast rounded-full inline-flex items-center justify-center"> -->
+    <!--     <i :class="upDownIcon" /> -->
+    <!--   </span> -->
+    <!-- </section> -->
+
+    <section>
+      <div class="flex flex-row items-center justify-around">
+        <img :src="'https://capytale2.ac-paris.fr/logo.svg'" class="w-40 mr-4 rounded-lg my-8" />
+        <div>
+          <h1>Capytale</h1>
+          <h2>
+            Activités numériques et de programmation pour la classe
+          </h2>
+        </div>
+      </div>
+      <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-2 gap-8 mb-4">
+        <Card v-for="(c, index) of cards" key="index">
+          <template #content>
+            <div class="flex flex-row align-center">
+              <i :class="c.icon" class="mt-2 mr-2 text-gray-500"></i>
+              <div class="font-semibold">{{ c.title }}</div>
+            </div>
+            <div class="mt-2 text-gray-500" v-html="c.description"></div>
+          </template>
+        </Card>
+      </div>
+      <span @click="nextSnap" class="slidedown-icon bg-primary text-primary-contrast flex items-center justify-center">
+        <i class="pi pi-arrow-circle-down" style="font-size: 1.5rem" />
+      </span>
+    </section>
+
+    <section>
+      <div class="flex flex-row items-center justify-around">
+        <img :src="'https://capytale2.ac-paris.fr/logo.svg'" class="w-40 mr-4 rounded-lg my-20" />
+        <div>
+          <h2>Créer, distribuer et évaler des activités pédagogiques</h2>
+        </div>
+      </div>
+      <CapyTimeline />
+      <span @click="nextSnap" class="slidedown-icon bg-primary text-primary-contrast flex items-center justify-center">
+        <i class="pi pi-arrow-circle-down" style="font-size: 1.5rem" />
+      </span>
+    </section>
+
+    <section>
+      <div class="flex flex-row items-center justify-around">
+        <img :src="'https://capytale2.ac-paris.fr/logo.svg'" class="w-40 mr-4 rounded-lg my-20" />
+        <div>
+          <h2>Une sélection d'outils complète et cohérente</h2>
+        </div>
+      </div>
+      <CarouselEdu />
+      <span @click="nextSnap" class="slidedown-icon bg-primary text-primary-contrast flex items-center justify-center">
+        <i class="pi pi-arrow-circle-up" style="font-size: 1.5rem" />
+      </span>
+    </section>
+  </article>
+</template>
+
+
 
 <style scoped>
 @keyframes slidedown-icon {
@@ -228,7 +225,7 @@ const nextSnap = () => {
 
 .slidedown-icon {
   animation: slidedown-icon;
-  animation-duration: 1s;
+  animation-duration: 2s;
   animation-iteration-count: infinite;
   cursor: pointer;
 }
@@ -236,7 +233,7 @@ const nextSnap = () => {
 .scroller {
   height: calc(100vh - 8rem);
   overflow-y: scroll;
-  scroll-snap-type: y proximity;
+  scroll-snap-type: y mandatory;
 }
 
 .scroller section {
