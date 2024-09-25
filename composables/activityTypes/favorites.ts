42650290765b5ed050f3044b358bec7c70afa3ac
tjaisson <thomas.jaisson@ac-paris.fr>
2024-09-25 16:13:13 +0200

    ActivityTypes: disponible hors composable

--- composables/activityTypes/favorites.ts
+++ composables/activityTypes/favorites.ts
@@ -1,10 +1,10 @@
 import { ref, shallowRef, computed, readonly } from 'vue';
 
-import typeApi, { type ActivityType, type ActivityGroups } from '@capytale/activity.js/backend/capytale/activityType';
+import typeApi from '@capytale/activity.js/backend/capytale/activityType';
 
 import { useActivityTypesList } from './list';
 
-let atl: ReturnType<typeof useActivityTypesList> | null = null;
+let atl = useActivityTypesList(true);
 
 const status = shallowRef<'loading' | 'loaded' | 'error'>('loading');
 const error = shallowRef<any>();
@@ -12,32 +12,35 @@ const favorites = ref<{ [t: string]: boolean }>({});
 const showFavorites = shallowRef<boolean>(false);
 let fetchPromise: Promise<void> | null = null;
 
-function reload(): void {
-  if (atl == null) {
-    atl = useActivityTypesList();
-  } else {
-    atl.reload();
-  }
-  if (fetchPromise != null) return;
-  if (typeApi.showFavorites()) {
-    status.value = 'loading';
-    error.value = null;
-    fetchPromise = typeApi.getFavorites()
-      .then((fa) => {
-        updateFavorites(fa);
-        showFavorites.value = true;
-        status.value = 'loaded';
-      })
-      .catch((e) => {
-        error.value = e;
-        status.value = 'error';
-      })
-      .finally(() => {
-        fetchPromise = null;
-      });
-  } else {
-    status.value = 'loaded';
-    error.value = null;
+/**
+ * Charge les favoris des types d'activité.
+ * 
+ * @param forceReload force le rechargement des favoris des types d'activité même si ils sont déjà chargés
+ */
+function load(forceReload: boolean = false): void {
+  atl.load();
+  if (forceReload || (status.value !== 'loaded')) {
+    if (fetchPromise != null) return;
+    if (typeApi.showFavorites()) {
+      status.value = 'loading';
+      error.value = null;
+      fetchPromise = typeApi.getFavorites()
+        .then((fa) => {
+          updateFavorites(fa);
+          showFavorites.value = true;
+          status.value = 'loaded';
+        })
+        .catch((e) => {
+          error.value = e;
+          status.value = 'error';
+        })
+        .finally(() => {
+          fetchPromise = null;
+        });
+    } else {
+      status.value = 'loaded';
+      error.value = null;
+    }
   }
 }
 
@@ -91,15 +94,24 @@ async function toggleFavorite(t: string): Promise<void> {
 
 const favoriteTypes = computed(() => atl!.availableTypes.filter(t => isFavorite(t)));
 
-export const useActivityTypeFavorites = () => {
-  if (status.value !== 'loaded') reload();
+function buildStore() {
   return readonly({
-    reload,
+    load,
     showFavorites,
     isFavorite,
     toggleFavorite,
     favoriteTypes,
     status: compStatus,
     error: compError,
-  })
+  });
 }
+
+type Store = ReturnType<typeof buildStore>;
+let store: Store | null = null;
+
+function useActivityTypeFavorites(lazy: boolean = false) {
+  if (!lazy) load();
+  return store ?? (store = buildStore());
+}
+
+export { useActivityTypeFavorites };
--- composables/activityTypes/weights.ts
+++ composables/activityTypes/weights.ts
@@ -8,45 +8,62 @@ const groups = shallowRef<ActivityGroup[]>([]);
 const all = shallowRef<{ [type: string]: number }>({});
 let fetchPromise: Promise<void> | null = null;
 
-function reload(): void {
-  if (fetchPromise != null) return;
-  status.value = 'loading';
-  error.value = null;
-  fetchPromise = typeApi.getWeights()
-    .then((w) => {
-      const gg: ActivityGroup[] = [];
-      for (const g in w.groups) {
-        gg.push(w.groups[g]);
-      }
-      groups.value = gg;
-      const aa: { [type: string]: number } = {};
-      for (const a in w.all) {
-        aa[a] = w.all[a];
-      }
-      all.value = aa;
-      status.value = 'loaded';
-    })
-    .catch((e) => {
-      error.value = e;
-      status.value = 'error';
-    })
-    .finally(() => {
-      fetchPromise = null;
-    });
+/**
+ * Charge les poids des types d'activité.
+ * 
+ * @param forceReload force le rechargement des poids des types d'activité même si ils sont déjà chargés
+ */
+function load(forceReload: boolean = false): void {
+  if (forceReload || (status.value !== 'loaded')) {
+    if (fetchPromise != null) return;
+    status.value = 'loading';
+    error.value = null;
+    fetchPromise = typeApi.getWeights()
+      .then((w) => {
+        const gg: ActivityGroup[] = [];
+        for (const g in w.groups) {
+          gg.push(w.groups[g]);
+        }
+        groups.value = gg;
+        const aa: { [type: string]: number } = {};
+        for (const a in w.all) {
+          aa[a] = w.all[a];
+        }
+        all.value = aa;
+        status.value = 'loaded';
+      })
+      .catch((e) => {
+        error.value = e;
+        status.value = 'error';
+      })
+      .finally(() => {
+        fetchPromise = null;
+      });
+  }
 }
 
-function load() {
-  if (status.value !== 'loaded') reload();
-}
-
-export const useActivityTypeWeights = (lazy: boolean = false) => {
-  if (!lazy && (status.value !== 'loaded')) reload();
+function buildStore() {
   return readonly({
-    reload,
     load,
     groups,
     all,
     status,
     error,
-  })
+  });
 }
+
+type Store = ReturnType<typeof buildStore>;
+let store: Store | null = null;
+
+/**
+ * Fournit un objet réactif contenant les poids et groupes des types d'activité. 
+ * 
+ * @param lazy retarde le chargement depuis le backend
+ * @returns 
+ */
+function useActivityTypeWeights(lazy: boolean = false): Store {
+  if (!lazy) load();
+  return store ?? (store = buildStore());
+}
+
+export { useActivityTypeWeights };
