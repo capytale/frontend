0103a11e2c0eb7e76c27edf7263a7554f92f8998
npoulain <nicolas.poulain@ac-paris.fr>
2024-11-18 15:51:39 +0100

    UserStore: passage de option à setup

--- stores/currentUser.ts
+++ stores/currentUser.ts
@@ -1,26 +1,28 @@
 import { loadCurrentUser, type CurrentUser } from "@/utils/currentUser";
 
-export const useCurrentUserStore = defineStore('user', {
-  state: () => ({
-    user: false
-  }) as {
-    user: CurrentUser | null | false;
-  },
-  actions: {
-    async getUser() {
-      this.user = await loadCurrentUser();
-    },
-  },
-  getters: {
-    isLoaded: (state) => state.user !== false,
-    isAuthenticated: (state) => (state.user != null && state.user !== false),
-    isTeacher: (state) => {
-      if (state.user == null || state.user === false) return false;
-      return state.user.profil === 'teacher';
-    },
-    isStudent: (state) => {
-      if (state.user == null || state.user === false) return false;
-      return state.user.profil === 'student';
-    },
+export const useCurrentUserStore = defineStore('user', () => {
+  const user = ref<CurrentUser | null | false>(false)
+  const getUser = async () => {
+    user.value = await loadCurrentUser();
+  }
+
+  const isLoaded = computed(() => user.value !== false)
+  const isAuthenticated = computed(() => (user.value != null && user.value !== false))
+  const isTeacher = computed(() => {
+    if (user.value == null || user.value === false) return false;
+    return user.value.profil === 'teacher';
+  })
+  const isStudent = computed(() => {
+    if (user.value == null || user.value === false) return false;
+    return user.value.profil === 'student';
+  })
+
+  return {
+    user,
+    getUser,
+    isLoaded,
+    isAuthenticated,
+    isTeacher,
+    isStudent,
   }
 })
