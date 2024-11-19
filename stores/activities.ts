32214cb7b76790716ed5eb468be6edfaf9b23ef4
Mathieu NICOLAS <mathieu@Mathieus-MacBook-Pro.local>
2024-11-19 10:14:38 +0100

    activities store : options vers setup

--- stores/activities.ts
+++ stores/activities.ts
@@ -17,220 +17,222 @@ type ActivityMetadata = {
   themes: number[];
 }
 
-export const useActivitiesStore = defineStore('activities', {
-  state: () => ({
-    activities: [],
-  }),
-  actions: {
-    async getActivities() {
-      // await new Promise(resolve => setTimeout(resolve, 3000))
-      this.activities = await fetchMyActivities()
+export const useActivitiesStore = defineStore('activities', () => {
+  const activities = ref<any[]>([])
+  const getActivities = async () => {
+    activities.value = await fetchMyActivities()
+  }
 
-    },
-    getMetadata(nid: number | string) {
-      return httpClient.getJsonAsync<ActivityMetadata>(metaDataEp(nid));
-    },
+  const getMetadata = (nid: number | string) => {
+    return httpClient.getJsonAsync<ActivityMetadata>(metaDataEp(nid));
+  }
 
-    async putMetaData(
-      nid: number | string,
-      data: ActivityMetadata
-    ) {
-      const act = this.getActivity(nid);
-      let currentShared: boolean | undefined;
-      let currentWeb: boolean | undefined;
+  const putMetaData = async (
+    nid: number | string,
+    data: ActivityMetadata
+  ) => {
+    const act = activities.value.find((a) => a.nid == nid);
+    let currentShared: boolean | undefined;
+    let currentWeb: boolean | undefined;
+    if (act != null) {
+      // optimistic update
+      currentShared = act.status_shared;
+      currentWeb = act.status_web;
+      act.status_shared = data.status_shared;
+      act.status_web = data.status_web;
+    }
+    try {
+      await httpClient.putJsonAsync(
+        metaDataEp(nid),
+        data
+      );
+    } catch (e) {
+      // Rollback
       if (act != null) {
-        // optimistic update
-        currentShared = act.status_shared;
-        currentWeb = act.status_web;
-        act.status_shared = data.status_shared;
-        act.status_web = data.status_web;
+        act.status_shared = currentShared!;
+        act.status_web = currentWeb!;
       }
-      try {
-        await httpClient.putJsonAsync(
-          metaDataEp(nid),
-          data
-        );
-      } catch (e) {
-        // Rollback
-        if (act != null) {
-          act.status_shared = currentShared!;
-          act.status_web = currentWeb!;
+      throw e;
+    }
+  }
+
+  const getActivity = (nid: number | string) => {
+    return activities.value.find((a) => a.nid == nid)
+  }
+
+  const getAllDetails = (activity: any) => {
+    if (activity.refreshRequested) return;
+    if (activity.extra) return;
+    if (activity.detailsRequested) return;
+    activity.detailsRequested = true;
+    httpClient.getJsonAsync("/web/c-hdls/api/all-details/" + activity.nid).then((details: any) => {
+      delete activity.detailsRequested
+      if (activity.refreshRequested) return
+      for (const key in details) activity[key] = details[key]
+      activity.extra = true
+    })
+  }
+
+  const refreshDetails = (activity: any) => {
+    if (activity.refreshRequested) return;
+    activity.refreshRequested = true;
+    httpClient.getJsonAsync("/web/c-hdls/api/refresh-details/" + activity.nid).then((details: any) => {
+      delete activity.refreshRequested
+      for (const key in details) activity[key] = details[key]
+      activity.extra = true
+    })
+    if (!activity.isSa) {
+      activities.value.forEach((a) => {
+        if ((a.isSa) && (a.activityId == activity.nid)) {
+          refreshDetails(a)
         }
-        throw e;
-      }
-    },
-
-
-    getActivity(nid: number | string) {
-      return this.activities.data.find((a) => a.nid == nid)
-    },
-
-    getAllDetails(activity: any) {
-      if (activity.refreshRequested) return;
-      if (activity.extra) return;
-      if (activity.detailsRequested) return;
-      activity.detailsRequested = true;
-      httpClient.getJsonAsync("/web/c-hdls/api/all-details/" + activity.nid).then((details: any) => {
-        delete activity.detailsRequested
-        if (activity.refreshRequested) return
-        for (const key in details) activity[key] = details[key]
-        activity.extra = true
       })
-    },
+    }
+  }
 
-    /**
-     * Mets à jour les champs de l'activité passée en paramètre
-     * Si l'utilisateur a un studentAssignment lié à l'activité, il est mis à jour
-     */
-    refreshDetails(activity: any) {
-      if (activity.refreshRequested) return;
-      activity.refreshRequested = true;
-      httpClient.getJsonAsync("/web/c-hdls/api/refresh-details/" + activity.nid).then((details: any) => {
-        delete activity.refreshRequested
-        for (const key in details) activity[key] = details[key]
-        activity.extra = true
-      })
-      if (!activity.isSa) {
-        this.activities.data.forEach((a) => {
-          if ((a.isSa) && (a.activityId == activity.nid)) {
-            this.refreshDetails(a)
-          }
-        })
-      }
-    },
+  const deleteActivity = async (nids: Array<number[]>) => {
+    const { deleted, notDeleted } = await httpClient.postGetJsonAsync<{ deleted: number[], notDeleted: number[] }>(
+      myActivitiesApiEp,
+      { action: "delete", nids }
+    );
+    for (let nid of deleted) {
+      activities.value = activities.value.filter((item) => item.nid !== nid);
+    }
+    return notDeleted
+  }
 
-    async deleteActivity(nids: Array<number[]>) {
-      const { deleted, notDeleted } = await httpClient.postGetJsonAsync<{ deleted: number[], notDeleted: number[] }>(
+  const cloneActivity = async (nid: number | string, title?: string) => {
+    let r
+    if (title === undefined) {
+      title = ""
+    }
+    try {
+      r = await httpClient.postGetJsonAsync<any>(
         myActivitiesApiEp,
-        { action: "delete", nids }
-      );
-      for (let nid of deleted) {
-        // console.log("deleteActivity", nid)
-        this.activities.data = this.activities.data.filter((item) => item.nid !== nid);
-      }
-      return notDeleted
-    },
+        { action: "clone", nid, title }
+      )
+    } catch (e) {
+      // console.log("error", e)
+    }
+    r.access_tr_mode = "none"
+    r.aid = r.nid
+    r.appreciation = ""
+    r.bib = `${r.nid}:0`
+    r.evaluation = ""
+    r.last_access = `${r.changed}`
+    r.mode = "N_O"
+    r.own = 1
+    r.own_aid = r.nid
+    r.player = `/web/c-act/n/${r.nid}/play/create`
+    r.row_id = `row-${r.nid}`
+    r.sa = "0"
+    r.status_clonable = 1
+    r.status_shared = "0"
+    r.status_web = "0"
+    r.tags = r.tags.map((tag) => tag.target_id) // il faut modifier un peu
+    r.views_hidden = 0
+    r.views_total = 0
+    r.whoami = "cr"
+    r.workflow = "0"
+    activities.value.push(r)
+    return r
+  }
 
-    async cloneActivity(nid: number | string, title?: string): Promise<{ nid: number | string, title: string }> {
-      let r
-      if (title === undefined) {
-        title = ""
-      }
-      try {
-        r = await httpClient.postGetJsonAsync<any>(
-          myActivitiesApiEp,
-          { action: "clone", nid, title }
-        )
-      } catch (e) {
-        // console.log("error", e)
-      }
-      // console.log("le clone : ", r.changed, r.code, r.nid, r.title, r.type, r.tags)
-      // On ajoute le clone au store
-      console.log("r", r)
-      r.access_tr_mode = "none"
-      r.aid = r.nid
-      r.appreciation = ""
-      r.bib = `${r.nid}:0`
-      r.evaluation = ""
-      r.last_access = `${r.changed}`
-      r.mode = "N_O"
-      r.own = 1
-      r.own_aid = r.nid
-      r.player = `/web/c-act/n/${r.nid}/play/create`
-      r.row_id = `row-${r.nid}`
-      r.sa = "0"
-      r.status_clonable = 1
-      r.status_shared = "0"
-      r.status_web = "0"
-      r.tags = r.tags.map((tag) => tag.target_id) // il faut modifier un peu
-      r.views_hidden = 0
-      r.views_total = 0
-      r.whoami = "cr"
-      r.workflow = "0"
-      if (this.activities.status === "success") {
-        this.activities.data.push(r)
-      }
-      return r
-    },
-
-    async lockMode(activity: any) {
-      // Optimistic update
-      const oldMode = activity.mode
-      activity.mode = "N_X"
-      try {
-        await httpClient.postJsonAsync(
-          myActivitiesApiEp,
-          { action: "lockMode", nid: activity.nid }
-        );
-      }
-      catch (e) {
-        // Rollback
-        activity.mode = oldMode
-        throw e
-      }
-    },
-
-    async unlockMode(activity: any) {
-      // Optimistic update
-      const oldMode = activity.mode
-      activity.mode = "N_O"
-      try {
-        await httpClient.postJsonAsync(
-          myActivitiesApiEp,
-          { action: "unlockMode", nid: activity.nid }
-        );
-      }
-      catch (e) {
-        // Rollback
-        activity.mode = oldMode
-        throw e
-      }
-    },
-
-    async moveActivities(pxyNids: array, tid: number) {
-      for (let item of pxyNids) {
-        this.activities.data = this.activities.data.map(el => el.nid == item.nid ? { ...el, tags: [tid] } : el);
-      }
-      let nids = [...pxyNids.map((o) => o.nid)];
+  const lockMode = async (activity: any) => {
+    // Optimistic update
+    const oldMode = activity.mode
+    activity.mode = "N_X"
+    try {
       await httpClient.postJsonAsync(
         myActivitiesApiEp,
-        { action: "moveFolder", nids, tid }
+        { action: "lockMode", nid: activity.nid }
       );
-    },
+    }
+    catch (e) {
+      // Rollback
+      activity.mode = oldMode
+      throw e
+    }
+  }
 
-    async untagActivity(nid: number, tid: number) {
-      let obj = this.activities.data.find(el => el.nid == nid);
-      let arrayTids = obj.tags;
-      arrayTids = arrayTids.filter(item => item !== tid);
-      this.activities.data = this.activities.data.map(el => el.nid == nid ? { ...el, tags: arrayTids } : el);
+  const unlockMode = async (activity: any) => {
+    // Optimistic update
+    const oldMode = activity.mode
+    activity.mode = "N_O"
+    try {
       await httpClient.postJsonAsync(
         myActivitiesApiEp,
-        { action: "untag", nid, tid }
+        { action: "unlockMode", nid: activity.nid }
       );
-    },
+    }
+    catch (e) {
+      // Rollback
+      activity.mode = oldMode
+      throw e
+    }
+  }
 
-    async changeMyVueCount(nid: number, a, b, c, d) {
-      // console.log("archive", nid, a, b, c, d)
-      this.activities.data = this.activities.data.map(el => el.nid == nid ? { ...el, viewsDetails: { 100: a, 200: b, 300: c, visible: d } } : el);
-    },
+  const moveActivities = async (pxyNids: array, tid: number) => {
+    for (let item of pxyNids) {
+      activities.value = activities.value.map(el => el.nid == item.nid ? { ...el, tags: [tid] } : el);
+    }
+    let nids = [...pxyNids.map((o) => o.nid)];
+    await httpClient.postJsonAsync(
+      myActivitiesApiEp,
+      { action: "moveFolder", nids, tid }
+    );
+  }
 
-    async bulkArchive(nids: number, corbeilleTid: number) {
-      for (let nid of nids) {
-        this.activities.data = this.activities.data.map(el => el.nid == nid ? { ...el, viewsDetails: { 100: 0, 200: 0, 300: 0, visible: 0 } } : el);
-        await httpClient.postJsonAsync(
-          myActivitiesApiEp,
-          { action: "bulkArchive", nid, corbeilleTid }
-        );
-      }
-    },
+  const untagActivity = async (nid: number, tid: number) => {
+    let obj = activities.value.find(el => el.nid == nid);
+    let arrayTids = obj.tags;
+    arrayTids = arrayTids.filter(item => item !== tid);
+    activities.value = activities.value.map(el => el.nid == nid ? { ...el, tags: arrayTids } : el);
+    await httpClient.postJsonAsync(
+      myActivitiesApiEp,
+      { action: "untag", nid, tid }
+    );
+  }
 
-    async replaceTags(nids: any[], tids: any[]) {
+  const changeMyVueCount = (nid: number, a, b, c, d) => {
+    activities.value = activities.value.map(el => el.nid == nid ? { ...el, viewsDetails: { 100: a, 200: b, 300: c, visible: d } } : el);
+  }
+
+  const bulkArchive = async (nids: number, corbeilleTid: number) => {
+    for (let nid of nids) {
+      activities.value = activities.value.map(el => el.nid == nid ? { ...el, viewsDetails: { 100: 0, 200: 0, 300: 0, visible: 0 } } : el);
       await httpClient.postJsonAsync(
         myActivitiesApiEp,
-        { action: "replaceTags", nids, tids }
+        { action: "bulkArchive", nid, corbeilleTid }
       );
-    },
-  },
+    }
+  }
+
+  const replaceTags = async (nids: any[], tids: any[]) => {
+    await httpClient.postJsonAsync(
+      myActivitiesApiEp,
+      { action: "replaceTags", nids, tids }
+    );
+  }
+
+  return {
+    activities,
+    getActivities,
+    getMetadata,
+    putMetaData,
+    getActivity,
+    getAllDetails,
+    refreshDetails,
+    deleteActivity,
+    cloneActivity,
+    lockMode,
+    unlockMode,
+    moveActivities,
+    untagActivity,
+    changeMyVueCount,
+    bulkArchive,
+    replaceTags,
+  }
 })
 
 // TODO : peaufiner la gestion de la récupération des favoris : pour l'instant, c'est géré dans les composants.
